24baeb81169401630da8efe39d8b222ea7e4954d
ArgenisPX <argenispx@gmail.com>
2019-12-24 02:04:16 -0400

    nuevo  estilo de alerta (beta)

--- src/container/inicioSesion/LoginRedes.js
+++ src/container/inicioSesion/LoginRedes.js
@@ -1,8 +1,15 @@
-import React from "react";
-import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
+import React, {Component} from "react";
+import { StyleSheet, Text, View, TouchableOpacity, Image,Modal } from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
 
-export default function LoginRedes() {
+export default class LoginRedes extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalVisible:false,
+    }}
+  render(){
+  
   return (
     <View style={styles.container}>
       <TouchableOpacity>
@@ -18,7 +25,7 @@ export default function LoginRedes() {
       </TouchableOpacity>
 
       <View style={styles.viewContainerF}>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={()=>{this.setState({modalVisible:!this.state.modalVisible})}}>
           <Text style={styles.facebook}>
             <FontAwesome name="facebook" size={20} color="#fff" /> Iniciar con
             Facebook
@@ -29,9 +36,27 @@ export default function LoginRedes() {
       <View style={{ alignItems: "center", margin: 5 }}>
         <Text style={{ fontSize: 18, color: "#00425A" }}>o</Text>
       </View>
+      <Modal
+        animationType="none"
+        transparent={true}
+        visible={this.state.modalVisible}
+        >
+        <View style={{flex: 1, alignItems:'center', justifyContent: 'center', backgroundColor:'white', opacity:0.9}}></View>
+        
+          <View style={{width:'80%', height:'60%', backgroundColor:"white", borderRadius:20,borderColor:"#0097CD", borderWidth:10,position:'absolute',marginTop:'40%',marginLeft:'10%'}}>
+          <TouchableOpacity onPress={()=>{this.setState({modalVisible:!this.state.modalVisible})}}
+            style={{width:'100%',height:'100%', justifyContent: "center",}}>
+              <Text style={{fontSize:30, color:"#0097CD", textAlign:'center',textShadowRadius: 2,fontFamily:'sans-serif-medium'}}
+              >Lo sentimos, esta funcion no se encuentra disponible.</Text> 
+
+          </TouchableOpacity>
+          
+        </View>
+        </Modal>
+        
     </View>
   );
-}
+}}
 
 const styles = StyleSheet.create({
   container: {
