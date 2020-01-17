a4d747a575e5c731e9ee6045f04f571f9d136b70
Carl Lewis <carllewiscastilo@gmail.com>
2020-01-16 21:48:37 -0400

    Gallery

--- src/container/inicioSesion/Formulario.js
+++ src/container/inicioSesion/Formulario.js
@@ -34,8 +34,7 @@ export default class Formulario extends Component {
   }
 
   async login(email, pass) {
-    email = 'lewis@gmail.com'
-    pass = '123456'
+
 
     if (!email) {
       // Alert.alert("Error", "El correo es Requerido para iniciar Sesion");
