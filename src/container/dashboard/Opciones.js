2294b5735ecc14d9f23b2b6db2ac73c0eafa0e4a
Carl Lewis <carllewiscastillo@gmail.com>
2021-05-29 18:56:23 -0400

    Component Options: Refactoring to functional component

--- src/container/dashboard/Opciones.js
+++ src/container/dashboard/Opciones.js
@@ -3,91 +3,91 @@ import { StyleSheet, View, ProgressBarAndroid } from 'react-native';
 import { Entypo, Foundation } from '@expo/vector-icons';
 import styled from 'styled-components/native';
 
-export default class Opciones extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { estado: false, Barra: 0, BarraP: 1 };
-  }
+const Options = (props) => {
+  const [estado, setEstado] = useState(false)
+  const [Barra, setBarra] = useState(0)
+  const [BarraP, setBarraP] = useState(1)
 
-  Barra1 = () => {
-    this.setState({ Barra: 1 });
-    this.setState({ BarraP: 0 });
-    this.setState({ estado: true });
-    this.props.CambiarEstado(true);
+  onBarra1 = () => {
+    setBarra(1);
+    setBarraP(0);
+    setEstado(true);
+    props.CambiarEstado(true);
   };
 
-  Barra2 = () => {
-    this.setState({ Barra: 0 });
-    this.setState({ BarraP: 1 });
-    this.setState({ estado: false });
-    this.props.CambiarEstado(false);
+  onBarra2 = () => {
+    setBarra(0);
+    setBarraP(1);
+    setEstado(false);
+    props.CambiarEstado(false);
   };
-  render() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.viewContainerGrup}>
-          <View style={styles.viewContainerInput}>
-            <View style={{ alignItems: 'center' }}>
-              {this.state.estado ? (
-                <Label onPress={this.Barra1}>
-                  <Foundation
-                    name="prohibited"
-                    size={18}
-                    color="rgb(255, 255, 255)"
-                  />{' '}
-                  Solicitados
-                </Label>
-              ) : (
-                <Label onPress={this.Barra1}>
-                  <Foundation
-                    name="prohibited"
-                    size={18}
-                    color="rgba(255, 255, 255, 0.5)"
-                  />{' '}
-                  Solicitados
-                </Label>
-              )}
-            </View>
 
-            <ProgressBarAndroid
-              styleAttr="Horizontal"
-              indeterminate={false}
-              color="#fff"
-              progress={this.state.Barra}
-            />
+  return (
+    <View style={styles.container}>
+      <View style={styles.viewContainerGrup}>
+        <View style={styles.viewContainerInput}>
+          <View style={{ alignItems: 'center' }}>
+            {estado ? (
+              <Label onPress={onBarra1}>
+                <Foundation
+                  name="prohibited"
+                  size={18}
+                  color="rgb(255, 255, 255)"
+                />{' '}
+                Solicitados
+              </Label>
+            ) : (
+              <Label onPress={onBarra1}>
+                <Foundation
+                  name="prohibited"
+                  size={18}
+                  color="rgba(255, 255, 255, 0.5)"
+                />{' '}
+                Solicitados
+              </Label>
+            )}
           </View>
 
-          <View style={styles.viewContainerInput}>
-            <View style={{ alignItems: 'center' }}>
-              {this.state.estado ? (
-                <Label onPress={this.Barra2}>
-                  <Entypo
-                    name="users"
-                    size={18}
-                    color="rgba(255, 255, 255, 0.5)"
-                  />{' '}
-                  Usuarios
-                </Label>
-              ) : (
-                <Label onPress={this.Barra2}>
-                  <Entypo name="users" size={18} color="rgb(255, 255, 255)" />{' '}
-                  Usuarios
-                </Label>
-              )}
-            </View>
+          <ProgressBarAndroid
+            styleAttr="Horizontal"
+            indeterminate={false}
+            color="#fff"
+            progress={Barra}
+          />
+        </View>
 
-            <ProgressBarAndroid
-              styleAttr="Horizontal"
-              indeterminate={false}
-              color="rgb(255, 255, 255)"
-              progress={this.state.BarraP}
-            />
+        <View style={styles.viewContainerInput}>
+          <View style={{ alignItems: 'center' }}>
+            {estado ? (
+              <Label onPress={onBarra2}>
+                <Entypo
+                  name="users"
+                  size={18}
+                  color="rgba(255, 255, 255, 0.5)"
+                />{' '}
+                Usuarios
+              </Label>
+            ) : (
+              <Label onPress={onBarra2}>
+                <Entypo name="users" size={18} color="rgb(255, 255, 255)" />{' '}
+                Usuarios
+              </Label>
+            )}
           </View>
+
+          <ProgressBarAndroid
+            styleAttr="Horizontal"
+            indeterminate={false}
+            color="rgb(255, 255, 255)"
+            progress={BarraP}
+          />
         </View>
       </View>
-    );
-  }
+    </View>
+  );
 }
+
+export default Options
 
 const Label = styled.Text`
   color: rgb(255, 255, 255);
@@ -107,15 +107,10 @@ const styles = StyleSheet.create({
   viewContainerGrup: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    // alignItems: 'stretch',
     marginTop: 20,
-    // marginBottom: 10,
-    // backgroundColor: '#fff',
     borderRadius: 15
   },
   viewContainerInput: {
-    // borderRadius: 15,
-    // backgroundColor: '#EBF2F4',
     width: 145,
     alignItems: 'stretch'
   }
